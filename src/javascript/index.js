c382c8b99146e6ef103337ca67e7a7720f47e01c
Chris <chrisoliver345@gmail.com>
2024-08-13 23:29:21 +1200

    Changed icon checkbox to slider using javascript class

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,40 +1,35 @@
 let dark_mode = false;
 
 function run_on_load() {
-    let switcher = document.querySelector("#theme-switch");
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        switcher.checked = true;
         dark_mode = true;
     }
-    update_color_scheme();
+    update_color_scheme(dark_mode);
 }
 
-function toggle_dark_mode() {
-    dark_mode = !dark_mode;
-    update_color_scheme();
+function toggle_dark_mode(isEnabled) {
+    dark_mode = isEnabled;
+    update_color_scheme(isEnabled);
 }
 
-function update_color_scheme() {
-    let icon = document.getElementById("icon");
-    if (dark_mode) {
-        icon.setAttribute("href", "/res/icon-transparent-square-inverted.svg?raw=true");
-    } else {
-        icon.setAttribute("href", "/res/icon-transparent-square.svg?raw=true");
-    }
+function update_color_scheme(isEnabled) {
+    const cell     = document.getElementById("switch").parentElement.parentElement;
+    cell.className = cell.className.substring(0, 23) + isEnabled;
 }
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', _event => {
     dark_mode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-    document.querySelector("#theme-switch").checked = dark_mode;
-    update_color_scheme();
+    update_color_scheme(dark_mode);
 });
 
 // themeSwitch.addEventListener('change', function (e) {
 //     if(e.currentTarget.checked === true) {
-//         // Add item to localstorage
+//         // Add item to localStorage
 //         localStorage.setItem('switchedTheme', 'true');
 //     } else {
 //         // Remove item if theme is switched back to normal
 //         localStorage.removeItem('switchedTheme');
 //     }
 // });
+
+//TODO: make theme persistent across reloads
